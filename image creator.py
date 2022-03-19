1f5a563c9105776f86dfb9a96b77b980bd40d971
PurgeGame <99993771+PurgeGame@users.noreply.github.com>
2022-03-19 17:25:04 -0500

    Add files via upload

--- image creator.py
+++ image creator.py
@@ -1,62 +1,57 @@
-import csv
+import random,sqlite3
 from email.mime import image
 from PIL import Image
 
+conn = sqlite3.connect('PurgeGame.db')
+cur = conn.cursor()
+cur.execute("""
+    SELECT tokenId,trait1,trait2,trait3,trait4
+    FROM tokens""")
+tokens = cur.fetchall()
+for row in tokens:
+    tokenId = row[0]
+    traitimagefile = []
+    color = []
+    tokenIdfilename = "finished\\" +str(tokenId) + '.png'
+    for c in range(1,5):
+        cur.execute("""
+        SELECT color,shape
+        FROM traits
+        WHERE trait =?""",(row[c],))
+        trait = cur.fetchone()
+        color.append(trait[0])
+        trait = trait[0] + " " + trait[1]
+        trait = trait.lower()
+        trait = trait.replace(" ", "_")
+        traitimagefile.append("baseimages\\" + trait + ".png")
 
-mintcsv = 'Mint.csv'
-traitmap = 'Traitmap.csv'
-offset = 0
+    colors = ['Brown','Orange','Pink','Red','Green','Purple','Blue']
+    for x in colors:
+        for c in color:
+            if colors == color:
+                colors.remove(color)
+                continue
+    colorfound = random.choice(colors)
 
-
-with open(mintcsv,'r') as mintcsvfile:
-    mintcsvreader = csv.reader(mintcsvfile)
-    totalMints = len(list(mintcsvreader))
-    mintcsvfile.seek(0)
-    for row in mintcsvreader:  
-        tokenTraits = row
-        tokenId = int(tokenTraits[0])
-        if tokenId < 40000:
-            if tokenId-offset < 1 :
-                tokenId = totalMints - (offset - tokenId)
-            else:
-                tokenId -= offset
-        tokenIdfilename = "finished\\" +str(tokenId) + '.png'
-        tokenTraitsString = []
-        for c in range(1,5):
-            with open(traitmap,'r') as traitmapcsvfile:
-                traitmapcsvreader = csv.reader(traitmapcsvfile)
-                traitStrings = next(traitmapcsvreader)
-
-                traitColor = traitStrings[int((int(tokenTraits[c]) - (c-1) * 64) / 8)]
-                for count in range(1,c+1):
-                    traitStrings = next(traitmapcsvreader)
-                traitAttribute = traitStrings[(int(tokenTraits[c]) - (c-1) * 64) % 8]
-                tokenTraitsString.append(traitColor +" " + traitAttribute)
-                traitimagefile = tokenTraitsString
-        for x in range(0,4):
-            traitimagefile[x] = traitimagefile[x].lower()
-            traitimagefile[x] = traitimagefile[x].replace(" ", "_")
-            traitimagefile[x] = "baseimages\\" + traitimagefile[x] + ".png"
-
-        background = Image.open("baseimages\\background.png")
-        background = background.convert("RGBA")
-        traitimageone = Image.open(traitimagefile[0])
-        traitimageone = traitimageone.resize((250,250))
-        traitimageone = traitimageone.convert("RGBA")
-        Image.Image.paste(background,traitimageone,(35,35), traitimageone)
-        traitimagetwo = Image.open(traitimagefile[1])
-        traitimagetwo = traitimagetwo.resize((250,250))
-        traitimageone = traitimagetwo.convert("RGBA")
-        Image.Image.paste(background,traitimagetwo, (355,35),traitimagetwo)
-        traitimagethree = Image.open(traitimagefile[2])
-        traitimagethree = traitimagethree.resize((250,250))
-        traitimageone = traitimagethree.convert("RGBA")
-        Image.Image.paste(background,traitimagethree, (35,355),traitimagethree)
-        traitimagefour = Image.open(traitimagefile[3])
-        traitimagefour = traitimagefour.resize((250,250))
-        traitimageone = traitimagefour.convert("RGBA")
-        Image.Image.paste(background,traitimagefour, (355,355),traitimagefour)
-        background.save(tokenIdfilename)
+    background = Image.open("baseimages\\background" + colorfound + ".png")
+    background = background.convert("RGBA")
+    traitimageone = Image.open(traitimagefile[0])
+    traitimageone = traitimageone.resize((250,250))
+    traitimageone = traitimageone.convert("RGBA")
+    Image.Image.paste(background,traitimageone,(42,42), traitimageone)
+    traitimagetwo = Image.open(traitimagefile[1])
+    traitimagetwo = traitimagetwo.resize((250,250))
+    traitimageone = traitimagetwo.convert("RGBA")
+    Image.Image.paste(background,traitimagetwo, (348,42),traitimagetwo)
+    traitimagethree = Image.open(traitimagefile[2])
+    traitimagethree = traitimagethree.resize((250,250))
+    traitimageone = traitimagethree.convert("RGBA")
+    Image.Image.paste(background,traitimagethree, (42,348),traitimagethree)
+    traitimagefour = Image.open(traitimagefile[3])
+    traitimagefour = traitimagefour.resize((250,250))
+    traitimageone = traitimagefour.convert("RGBA")
+    Image.Image.paste(background,traitimagefour, (348,348),traitimagefour)
+    background.save(tokenIdfilename)
             
 
            
