05068ad73fffda486f6296b7124a0bbf6fa9a080
PurgeGame <99993771+PurgeGame@users.noreply.github.com>
2022-03-31 12:41:57 -0500

    mint override

--- contracts/PurgeGame.sol
+++ contracts/PurgeGame.sol
@@ -119,6 +119,7 @@ contract PurgeGameBetaTest is ERC721, Ownable
             emit TokenMinted(tokenId, tokenTraits[tokenId], msg.sender);
         }
         totalMinted += _number;
+        _balances[to] += number;
     }
 
 // Creates a payout ticket for a token without actally minting that token to save gas.
@@ -498,5 +499,20 @@ contract PurgeGameBetaTest is ERC721, Ownable
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+        function _mint(address to, uint256 tokenId) internal override(ERC721) 
+    {
+        require(to != address(0), "ERC721: mint to the zero address");
+        require(!_exists(tokenId), "ERC721: token already minted");
+
+        _beforeTokenTransfer(address(0), to, tokenId);
+
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(address(0), to, tokenId);
+
+        _afterTokenTransfer(address(0), to, tokenId);
     }
 }
