cfa5b7d7d7be91d9808da12cf71bf328f609f750
PurgeGame <99993771+PurgeGame@users.noreply.github.com>
2022-04-09 03:28:40 -0500

    get address from env

--- scripts/metadata refresh.py
+++ scripts/metadata refresh.py
@@ -1,8 +1,11 @@
 import urllib.request, time
+import os
+
+address = os.environ.get("ADDRESS")
 
 for c in range(1,2000):
     print(c)
-    url ='https://rinkeby-api.opensea.io/api/v1/asset/0x3973A924Fdc47f7FA11565399A7d65Bb76d3D89a/' + str(c) + '/?force_update=true'
+    url ='https://rinkeby-api.opensea.io/api/v1/asset/' + address + '/' + str(c) + '/?force_update=true'
     page=urllib.request.Request(url,headers={'User-Agent': 'Mozilla/5.0'})
     urllib.request.urlopen(page)
     time.sleep(1)
